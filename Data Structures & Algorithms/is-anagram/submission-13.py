f5d2254e9815b7cb9b68f6b12baad1d9252d9436
Dinesh Reddy Thimmasani <thimmasanidineshreddy19@gmail.com>
2026-09-13 21:47:37 +0530

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return True if (tuple(sorted(s)) == tuple(sorted(t))) else False
+        #return ''.join(sorted(s)) == ''.join(sorted(t))
+        if (set(s)!=set(t)) or (len(s)!=len(t)) : return False
+        dict_s = {}
+        dict_t = {}
+        for i in range(len(s)):
+            if s[i] in dict_s : dict_s[s[i]]+=1
+            else: dict_s[s[i]]=1
+            if t[i] in dict_t : dict_t[t[i]]+=1
+            else: dict_t[t[i]]=1
+        return dict_s==dict_t
+
