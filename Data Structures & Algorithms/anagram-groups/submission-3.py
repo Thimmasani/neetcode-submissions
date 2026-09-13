dabb873d2488d21486df5c0ae4cc1f391f60b53a
Dinesh Reddy Thimmasani <thimmasanidineshreddy19@gmail.com>
2026-09-13 22:19:23 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for word in strs:
+            key = tuple(sorted(word))
+            hashmap[key].append(word)
+        return list(hashmap.values())
